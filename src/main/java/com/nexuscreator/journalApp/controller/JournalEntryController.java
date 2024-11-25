18635120321829f62d2c734e618ffc7bbdd72ad1
sakhawat777 <biplobbms@gmail.com>
2024-11-25 13:32:30 +0600

    Get and POST API made done

---
+++ src/main/java/com/nexuscreator/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,29 @@
+package com.nexuscreator.journalApp.controller;
+
+import com.nexuscreator.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries = new HashMap();
+    @GetMapping("/abc")
+
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+    @RequestMapping("/create")
+    public  boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+
+    }
+}
